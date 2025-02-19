74393248b7d402c48a5176211c4f4525b824d1e9
Kimizx3 <55560064+Kimizx3@users.noreply.github.com>
2025-02-18 22:16:14 -0800

    folder

---
+++ Assets/Scripts/Kimi/Expresso.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Expresso : MonoBehaviour
+{
+    // Use mouse to hover, change mouse icon
+    // Click cup and hold, cup image goes with mouse / stick to mouse
+    // Release mouse click, cup drop
+    // Move to coffee machine, create an outline to the machine to highlight it
+    // Drop cup on machine
+    // Progress bar starts loading
+    // Loading finished
+    // A cup of espresso will be created
+    // TODO: Create a prefab or a scriptable object for filled coffee
+}
