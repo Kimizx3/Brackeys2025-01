64d34222caff4670a47687edcbe81db5ca62c41f
WincyM <WincyM81323@163.com>
2025-03-02 12:07:44 +0800

    fix timeline -> sfx & dialog

--- Assets/Script/Timeline/TimelineBehavior.cs
+++ Assets/Script/Timeline/TimelineBehavior.cs
@@ -8,6 +8,9 @@ public class TimelineBehavior: MonoBehaviour
     public PlayableDirector director;
     public bool paused = false;
     public List<GameObject> sfxObjects;
+    public List<GameObject> sfxObjects_1;
+    public List<GameObject> sfxObjects_2;
+    public List<GameObject> sfxObjects_3;
 
     void Update()
     {
@@ -44,4 +47,54 @@ public class TimelineBehavior: MonoBehaviour
             }
         }
     }
+
+    public void PlaySFX1()
+    {
+        foreach (var gameObject in sfxObjects_1)
+        {
+            var audioSource1 = gameObject.GetComponent<AudioSource>();
+            if (audioSource1 != null)
+            {
+                audioSource1.Play();
+            }
+            else
+            {
+                Debug.LogError("AudioSource component missing on " + gameObject.name);
+            }
+        }
+    }
+
+    public void PlaySFX2()
+    {
+        foreach (var gameObject in sfxObjects_2)
+        {
+            var audioSource2 = gameObject.GetComponent<AudioSource>();
+            if (audioSource2 != null)
+            {
+                audioSource2.Play();
+            }
+            else
+            {
+                Debug.LogError("AudioSource component missing on " + gameObject.name);
+            }
+        }
+    }
+
+    public void PlaySFX3()
+    {
+        foreach (var gameObject in sfxObjects_3)
+        {
+            var audioSource3 = gameObject.GetComponent<AudioSource>();
+            if (audioSource3 != null)
+            {
+                audioSource3.Play();
+            }
+            else
+            {
+                Debug.LogError("AudioSource component missing on " + gameObject.name);
+            }
+        }
+    }
+
+    
 }
